dac8ec4e451782ed891f8e1d4a1495276ceade22
puro-ai <988snv9phn@privaterelay.appleid.com>
2026-06-19 11:25:38 +0800

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,13 @@
+const CACHE_NAME = 'wsl-summary-v9-machine-simple';
+const FILES = ['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
+self.addEventListener('install', event => {
+  self.skipWaiting();
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(FILES)));
+});
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))));
+  self.clients.claim();
+});
+self.addEventListener('fetch', event => {
+  event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
+});
